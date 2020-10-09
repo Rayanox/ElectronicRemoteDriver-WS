6ef74ea25f4834fb5bf6e7de4fce7cbb7cc183b9
Rayane BEN-HMIDANE <rayane.benhmidane32@hotmail.fr>
2020-10-09 15:15:14 +0200

    Add file of tests empty

---
+++ WS/src/test/java/main/CommandProviderTest.java
@@ -0,0 +1,34 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommandProviderTest {
+
+	@Test
+	public void testbuildCommandTest_1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testbuildCommandTest_2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void throwExceptionOnBadFormattedFile_1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void throwExceptionOnBadFormattedFile_2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void throwExceptionOnBadFormattedFile_3() {
+		fail("Not yet implemented");
+	}
+
+}
